371d0c50452c66c6ae66a2bdfb9537ac3349a14c
puerchik <rumyantsevnn@yandex.ru>
2023-08-29 11:49:40 +0300

    clock is done

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [time, setTime] = useState(new Date())
+
+  useEffect(() => {
+    setInterval(() => {
+      setTime(new Date())
+    }, 1000)
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="clockWrapper">
+        <span className='time'>{time.toLocaleTimeString()}</span>
+      </div>
     </div>
   );
 }
