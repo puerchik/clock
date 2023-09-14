b7d552bb6d6ae88720b9a30655acefc8a07c946d
puerchik <rumyantsevnn@yandex.ru>
2023-09-14 15:39:01 +0300

    minor edits

--- src/Components/StopClock.tsx
+++ src/Components/StopClock.tsx
@@ -6,7 +6,7 @@ export const StopClock: React.FC<StopClockPropsType> = ({changeToggle}) => {
     return (
         <>
             <div className="stopClock">
-                <span className="stopClockText">Stop clock</span>
+                <span className="stopClockText">Back to the clock press"return"</span>
                 <button onClick={changeToggle} className="button resetButton">return</button>
             </div>
         </>
